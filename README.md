07818b54c0d0ba91674573cf43aa8d3adec4e453
lizhigaomac <lizhigao@021.com>
2018-08-07 10:44:59 +0800

    update master

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # webpack-demos
+
 webpack-demos
 
+- [demo1]('')
+
 ## 环境
+
 - node v8.11.1
 - npm v5.6.0
 - nvm v0.33.11
--- demo1/src/index.js
+++
@@ -1,9 +0,0 @@
-function component() {
-  var element = document.createElement('div');
-
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
